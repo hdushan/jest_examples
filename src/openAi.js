92b864304b29230659aeb5775217a75e0b031fa9
Hans Dushanthakumar <hans.dushanthakumar@amaysim.com.au>
2025-04-20 20:54:58 +1000

    Update openAi.js

--- src/openAi.js
+++ src/openAi.js
@@ -7,7 +7,14 @@ export default async function openAi(message) {
 
   const response = await openaiClient.responses.create({
     model: 'gpt-4o',
-    input: message
+
+    // 'developer' messages provide the system's rules and business logic, like a function definition.
+    // user messages provide inputs and configuration to which the developer message instructions
+    // are applied, like arguments to a function.
+    input: [
+      { role: 'developer', content: 'Talk like a news reporter' },
+      { role: 'user', content: message }
+    ]
   })
 
   return response.output_text
