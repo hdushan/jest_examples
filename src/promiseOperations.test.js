1d484107d5e04a73decef6f0cc621c838bb60853
Hans Dushanthakumar <hansrd_98@yahoo.com>
2025-04-20 12:58:09 +1000

    Promise operation tests

--- src/promiseOperations.test.js
+++ src/promiseOperations.test.js
@@ -3,31 +3,31 @@ import { describe } from 'node:test'
 function promiseExample(name) {
   return new Promise((resolve, reject) => {
     if (name.toLowerCase() === 'hans') {
-      resolve()
+      resolve('me')
     } else {
-      reject()
+      reject('not me')
     }
   })
 }
 
 const promiseCaller = (name) => {
   return promiseExample(name)
-    .then(() => {
-      return 'resolved'
+    .then((returnedString) => {
+      return returnedString
     })
-    .catch(() => {
-      return 'rejected'
+    .catch((errorString) => {
+      return errorString
     })
 }
 
 describe('Promise Operations', () => {
   it('Promise example', async () => {
-    await expect(promiseExample('hans')).resolves.toBeUndefined()
-    await expect(promiseExample('peter')).rejects.toBeUndefined()
+    await expect(promiseExample('hans')).resolves.toBe('me')
+    await expect(promiseExample('peter')).rejects.toBe('not me')
   })
 
   it('Promise caller example', async () => {
-    expect(await promiseCaller('hans')).toBe('resolved')
-    expect(await promiseCaller('peter')).toBe('rejected')
+    expect(await promiseCaller('hans')).toBe('me')
+    expect(await promiseCaller('peter')).toBe('not me')
   })
 })
